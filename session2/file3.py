45317f14bb518b29cecb742149d50f4b3375f98b
PK/PY <144901752+pksenpai@users.noreply.github.com>
2024-11-12 14:22:42 +0330

    Nov 11

--- session2/file3.py
+++ session2/file3.py
@@ -1,6 +1,6 @@
-n=input("please neter your name?")
-y=int(input("please enter year of your birthday?"))
-x=int(input("please enter current year?"))
-age=x-y
-print("hello",n)
-print("you are",age,"years old")
+n = input("please neter your name?")
+y = int(input("please enter year of your birthday?"))
+x = int(input("please enter current year?"))
+age = x - y
+print("hello", n)
+print("you are", age, "years old")
