42c4d603e44d9548ec5bb52a554548f1a0498255
PK/PY <144901752+pksenpai@users.noreply.github.com>
2024-11-12 14:22:00 +0330

    Nov 11

--- session2/file2.py
+++ session2/file2.py
@@ -1,3 +1,3 @@
-a=float(input("please enter your required first number?"))
-b=float(input("please neter your required second number?"))
-print(a,"+",b,"=",a+b)
+a = float(input("please enter your required first number?"))
+b = float(input("please neter your required second number?"))
+print(a, "+", b, "=", a + b)
