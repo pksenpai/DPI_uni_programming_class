ce0a10694e96f837bd2bab63e3125ed1e979af1d
PK/PY <144901752+pksenpai@users.noreply.github.com>
2024-11-19 19:10:37 +0330

    Update file2.py

--- session3/file2.py
+++ session3/file2.py
@@ -1,10 +1,12 @@
-w=float(input("please enter width of your land?"))
-h=float(input("please enter height of your land?"))
-s=w*h
-if s>=300:   
+w = float(input("please enter width of your land?"))
+h = float(input("please enter height of your land?"))
+
+s = w * h
+
+if s >= 300:   
    print("your land is valuable")
-   p=float(input("please neter price of each meter?")) 
-   k=s*p
-   print("value of your land is",k)
+   p = float(input("please neter price of each meter?")) 
+   k = s * p
+   print("value of your land is", k)
 else:
    print("not good")
