7369f70094c254aa2118ebdc70b0c3d66cfb7436
PK/PY <144901752+pksenpai@users.noreply.github.com>
2024-11-19 19:11:33 +0330

    Update file4.py

--- session3/file4.py
+++ session3/file4.py
@@ -1,5 +1,6 @@
-x=input("please neter your password?")
-if len(x)>=4 and len(x)<=8 and not("a" in x):
+x = input("please neter your password?")
+
+if len(x) >= 4 and len(x) <= 8 and not ("a" in x):
    print("correct")
 else:
    print("wrong")
