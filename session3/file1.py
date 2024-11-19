85e62d02f8753f342351ef13f11df6b638454044
PKPY <p01001011p01011001@gmail.com>
2024-11-19 19:08:56 +0330

    Nov18

---
+++ session3/file1.py
@@ -0,0 +1,7 @@
+x=float(input("please enter your number?"))
+if x>10:
+   print("your number is more than 10")
+   print("---------------------------")
+else:
+   print("your number is less than 10")
+print("end")
---
+++ session3/file4.py
@@ -0,0 +1,5 @@
+x=input("please neter your password?")
+if len(x)>=4 and len(x)<=8 and not("a" in x):
+   print("correct")
+else:
+   print("wrong")
