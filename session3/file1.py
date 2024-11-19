007f57874758dcf3b8f78b782ac1fc534fdf4122
PK/PY <144901752+pksenpai@users.noreply.github.com>
2024-11-19 19:10:03 +0330

    Update file1.py

--- session3/file1.py
+++ session3/file1.py
@@ -1,7 +1,9 @@
-x=float(input("please enter your number?"))
-if x>10:
+x = float(input("please enter your number?"))
+
+if x > 10:
    print("your number is more than 10")
    print("---------------------------")
 else:
    print("your number is less than 10")
+
 print("end")
